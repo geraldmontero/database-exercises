de1359be207ae783890e3a0748ead4ebdf6a7cae
Gerald Montero <geraldmontero12@gmail.com>
2022-03-18 16:59:13 -0500

    update complete

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,8 +1,16 @@
 USE codeup_test_db;
 SELECT name AS 'all Albums' FROM albums;
 UPDATE albums
-SET sales = 'value1'* 10;
-WHERE sales = sales ;
+SET sales = sales * 10
+WHERE sales > 0 ;
 SELECT name AS 'All released before 1980' FROM albums WHERE release_date <= 1980;
+UPDATE albums
+SET release_date = release_date- 100
+WHERE release_date < 1980;
 SELECT name AS 'MJS albums' FROM albums WHERE artist = 'Michael Jackson';
-
+UPDATE albums
+set artist = 'Peter Jackson'
+WHERE artist = 'Michael Jackson';
+SELECT sales FROM albums;
+SELECT name FROM albums WHERE release_date < 1900;
+SELECT * FROM albums WHERE artist ='Peter Jackson';
